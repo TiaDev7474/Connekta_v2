38b68f8a5e76501dfe6a2c1cc4e3d291bf96e0f8
Nomenjanahary Riry <rirynomenjanhary@gmail.com>
2023-09-28 15:06:56 +0300

    fix:fix deploy error by deleting unused imported component

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,22 +1,16 @@
 import { MainLayout } from "@/components/Layout"
-import { useUser } from "@/features/auth/hooks/useUser"
-// import { useUserContext } from "@/hooks/useUserContext"
+// import { useUser } from "@/features/auth/hooks/useUser"
 import { Suspense} from "react"
-// import  { Navigate } from "react-router-dom"
 import { Outlet} from "react-router-dom"
 
 const Homepage  = () => {
-    // const { isAuthentified } = useUserContext();
+  
 
-    const {isError,  error } = useUser();
+    // const {isError,  error } = useUser();
    
     return (
         <MainLayout>
-            {/* {
-                !isAuthentified  && (
-                  <Navigate to="/auth/login" replace={true} />
-            )
-            } */}
+          
             <Suspense 
                 fallback={
                     <div>
